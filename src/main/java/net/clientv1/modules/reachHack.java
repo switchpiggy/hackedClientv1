884df36510e1f2f51bca2d27d6def40f948cb6ee
switchpiggy <switchpiggy@users.noreply.github.com>
2020-04-14 19:24:50 -0700

    reach hack

---
+++ src/main/java/net/clientv1/modules/reachHack.java
@@ -0,0 +1,9 @@
+package net.clientv1.modules;
+
+import net.clientv1.module;
+import org.lwjgl.input.Keyboard;
+
+@module.ModInfo(name = "reachHack", description = "Allows players to hit others from further away.", type = module.type.COMBAT, keyCode = Keyboard.KEY_R)
+
+public class reachHack extends module {
+}
